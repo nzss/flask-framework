3c1f1edc416707d4de6ba9d4f0a294465b5adfa0
Nilu Zhao <niluzhao@gmail.com>
2019-01-06 19:17:58 -0500

    clean up files

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/') # homepage
 def index():
-  return render_template('index2.html')
+  return render_template('index.html')
 
 @app.route('/handle_data', methods=['POST'])
 def handle_data():
